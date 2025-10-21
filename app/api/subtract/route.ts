a1b07d46b7ff8f080db550fd4f63a183222a1eb2
Diego <dtoscano@metropolitan-touring.com>
2025-10-20 20:14:52 -0500

    Cambios en imports

--- app/api/subtract/route.ts
+++ app/api/subtract/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { validatePayload, subtractNumbers } from "@/src/lib/math";
+import { validatePayload, subtractNumbers } from "../../../src/lib/math";
 
 export async function POST(req: Request) {
   try {
